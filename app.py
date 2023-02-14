d1ae1e016632a49ff10d95124fa9ef033fb1234d
liarar22 <idtnqhr@gmail.com>
2023-02-14 17:55:27 +0900

    ver_0.7

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 # extension에서 flask 설치하고, terminal에 pip install flask
-# 로그인 정보 db 연동을 위해 terminal에 pip install flask-mysqldb
+# 로그인 정보 db 연동을 위해 terminal에 pip install flask-sqlalchemy
 # https://streamls.tistory.com/entry/Flask%EA%B0%95%EC%A2%8C-%EB%A1%9C%EA%B7%B8%EC%9D%B8 : 로그인 방법 관련해서 이걸 챙겨보자
-from flask import Flask, render_template
+import os #절대경로 지정 위함
+from flask import Flask
+from flask import render_template
+from flask import request, redirect, session
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
@@ -9,7 +13,7 @@ app = Flask(__name__)
 def HF_CMS_01():
     return render_template(template_name_or_list = 'logo_page.html')
 
-@app.route(rule = '/login')
+@app.route(rule = '/login', method = ['GET', 'POST'])
 def HF_CMS_02():
     return render_template(template_name_or_list = 'login_page.html')
 
